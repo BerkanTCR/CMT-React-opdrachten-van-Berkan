db3185ffd74eea12ba30973dfcd6f33682414b01
[9025481@student.zadkine.nl] <9025481@student.zadkine.nl>
2025-10-08 23:09:08 +0200

    .

--- opdracht-01-books/src/components/BookList.jsx
+++ opdracht-01-books/src/components/BookList.jsx
@@ -4,7 +4,7 @@ const BookList = ({}) => {
 
     <section className="booklistsection">
         <Book title='Het leven van een loser' author='Jason' image="./images/book-1.png"/>
-        <Book title='killer instinct' author='Jason' image="./images/book-2.png"/>
+        <Book title='killer instinct' author='Berkan' image="./images/book-2.png"/>
         <Book title='Canon van Geneeskunde' author='Ibn Sina' image="./image/book-3.png"/>
     </section>
 
