64afd686c875fb4a149e7b36c0861ebfb265f62e
[9025481@student.zadkine.nl] <9025481@student.zadkine.nl>
2025-10-29 11:40:32 +0100

    kleine aanpassing

--- opdracht-01-books/src/components/BookList.jsx
+++ opdracht-01-books/src/components/BookList.jsx
@@ -3,8 +3,8 @@ const BookList = () => {
     return (
 
     <section className="booklistsection">
-        <Book title='Het leven van een loser' author='Jason' image="./images/book-1.png"/>
-        <Book title='killer instinct' author='Berkan' image="./images/book-2.png"/>
+        <Book title='Harry Potter' author='Lukasz' image="./images/book-1.png"/>
+        <Book title='Fantasia VI' author='Berkan' image="./images/book-2.png"/>
         <Book title='Canon van Geneeskunde' author='Ibn Sina' image="./images/canon.png"/>
     </section>
 
