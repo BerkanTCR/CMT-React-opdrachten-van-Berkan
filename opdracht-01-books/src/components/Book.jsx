c6710faa1d4d06a65a486be5321682c8807e6d89
[9025481@student.zadkine.nl] <9025481@student.zadkine.nl>
2025-11-10 14:11:09 +0100

    1.3

--- opdracht-01-books/src/components/Book.jsx
+++ opdracht-01-books/src/components/Book.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
 const Book = ({image, title, author}) => {
+       
+        const [aantalKeerGelezen, setAantalKeerGelezen] = useState(0)
+
+        function verhoogTeller () {
+            setAantalKeerGelezen(aantalKeerGelezen + 1)
+        }
+    
     return (
-    <section className="booksection">
-        <img src={image} alt="" />
-        <h2>{title}</h2>
-        <h3>{author}</h3>
-    </section>  
+    <>
+        <h1 className="bookteller">Dit boek is zo vaak gelezen: {aantalKeerGelezen}!</h1>
+            <section className="booksection">
+                <img src={image} alt="" />
+                <h2>{title}</h2>
+                <h3>{author}</h3>
+                <button onClick={verhoogTeller}>Keer gelezen:</button>
+            </section> </>
      );
+
 }
  
 export default Book;
