c167b5c61eb1c8522a95976ab5fde6d33c7c125a
[9025481@student.zadkine.nl] <9025481@student.zadkine.nl>
2026-01-06 22:21:39 +0100

    Barbershop

---
+++ opdracht-15-old-town-barber-app/src/components/ShopInfo.jsx
@@ -0,0 +1,11 @@
+const ShopInfo = () => {
+    return (
+        <h1>Shop Info</h1>
+        <h2>Old Town Barber</h2>
+        <h2>Openingstijden: </h2>
+        <h3>- Knipbeurt €25</h2>
+        <h3>- Baard Trim €15</h2>
+    );
+}
+
+export default ShopInfo;
---
+++ opdracht-15-old-town-barber-app/src/components/UserProfile.jsx
@@ -0,0 +1,8 @@
+const UserProfile = () => {
+    return (
+        <h1>Welkom bij Old Town Barber!</h1>
+
+    );
+}
+
+export default UserProfile;
