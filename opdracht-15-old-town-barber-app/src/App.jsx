4d60d3cd337a9d325604fddc18b627547eee632f
[9025481@student.zadkine.nl] <9025481@student.zadkine.nl>
2026-01-07 12:02:36 +0100

    UserProfile.jsx, localStorage.js, en importaties en code in de App.jsx

--- opdracht-15-old-town-barber-app/src/App.jsx
+++ opdracht-15-old-town-barber-app/src/App.jsx
@@ -1,10 +1,34 @@
-import './App.css';
-
+import { useState, useEffect } from "react";
+import { getUserProfile, setUserProfile } from "./utils/localStorage";
+import { toast } from "react-toastify";
+import UserProfile from './components/UserProfile'
 function App() {
+
+
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    const savedProfile = getUserProfile();
+    setProfile(savedProfile);
+  }, [])
+
+  const handleSave = (profileData) => {
+    setUserProfile(profileData);
+    setProfile(profileData);
+    toast.success('Profiel is aangemaakt');
+
+  }
+
+
+
+  // geen profiel? Laat het formulier zien -Jason
+  if (!profile) {
+    return <UserProfile saveProfile={handleSave} />
+  }
 
   return (
     <>
-      <h1>Opdracht 13 - Flashcard Quiz</h1>
+      <h1>Barbershop</h1>
     </>
   );
 }
--- opdracht-15-old-town-barber-app/src/components/UserProfile.jsx
+++ opdracht-15-old-town-barber-app/src/components/UserProfile.jsx
@@ -1,8 +1,51 @@
-const UserProfile = () => {
-    return (
-        <h1>Welkom bij Old Town Barber!</h1>
+import { useState } from "react";
+import { toast } from "react-toastify";
 
+const UserProfile = ({saveProfile}) => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if(!name.trim() || !email.trim() || !phone.trim()){
+            toast.error("Vul alle velden in!");
+            return;
+        }
+
+        saveProfile({name, email, phone})
+    }
+
+    return (
+
+        <section>
+            <h2>Jouw Profiel</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="naam"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+
+                <input
+                    type="text"
+                    placeholder="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <input type="text"
+                    placeholder="telefoon"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                />
+
+                <button type="submit">Profiel aanmaken</button>
+            </form>
+        </section>
     );
-}
+};
 
 export default UserProfile;
